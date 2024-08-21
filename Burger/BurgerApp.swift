353e426d8217f41077e76b4c6b4f2b05c1176e35
iSAD <bradx2713@gmail.com>
2024-08-21 11:41:24 +0200

    Initial Commit

---
+++ Burger/BurgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurgerApp.swift
+//  Burger
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/08/21.
+//
+
+import SwiftUI
+
+@main
+struct BurgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
